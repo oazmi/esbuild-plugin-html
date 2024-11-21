1bee62dfe76b6886b34fd0e8ee139a0e549f7dad
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-11-21 09:18:49 -0500

    - in `/src/deps.ts` update dependency `@oazmi/kitchensink` from `0.8.4` to `@0.8.5`. - in `/src/funcdefs.ts`, add function `predictEsbuildOutputPaths` that predicts the esbuild output paths of a given collection of entry points.   this information will be needed to embed back the dependency entry points of an html file.

--- src/deps.ts
+++ src/deps.ts
@@ -1,16 +1,24 @@
-import { getUriScheme, resolvePathFactory } from "jsr:@oazmi/kitchensink@0.8.4/pathman"
+import { forbiddenBaseUriSchemes, getUriScheme, resolvePathFactory, uriProtocolSchemeMap } from "jsr:@oazmi/kitchensink@0.8.5/pathman"
 
 
 export { DOMParser } from "jsr:@b-fuze/deno-dom@0.1.48"
-export { object_entries, object_fromEntries, object_keys, object_values } from "jsr:@oazmi/kitchensink@0.8.4/builtin_aliases_deps"
-export { getUriScheme, joinPaths, resolveAsUrl } from "jsr:@oazmi/kitchensink@0.8.4/pathman"
-// TODO: in the future, we will have to wrap `getUriScheme` and export a different `getUriScheme` that understands our custom `inline://` uri-scheme
+export { object_entries, object_fromEntries, object_keys, object_values, promise_all, json_parse, json_stringify } from "jsr:@oazmi/kitchensink@0.8.5/alias"
+export { getUriScheme, joinPaths, relativePath, resolveAsUrl } from "jsr:@oazmi/kitchensink@0.8.5/pathman"
+export { isArray, isString } from "jsr:@oazmi/kitchensink@0.8.5/struct"
+export type * as esbuild from "npm:esbuild"
+
+// DONE: in the future, we will have to wrap `getUriScheme` and export a different `getUriScheme` that understands our custom `inline://` uri-scheme
+uriProtocolSchemeMap.push(
+	["link://", "link" as any],
+	["inline://", "inline" as any],
+)
+forbiddenBaseUriSchemes.push("link" as any, "inline" as any)
 
 // below is a custom path segment's absoluteness test function that will identify all `UriScheme` segments that are not "relative" as absolute paths,
-const absolute_path_segment_tester = (segment: string) => {
+export const isAbsolutePath = (segment: string) => {
 	return getUriScheme(segment) !== "relative"
 }
 
 export const
 	getCwd = () => Deno.cwd(),
-	resolvePath = resolvePathFactory(getCwd, absolute_path_segment_tester)
+	resolvePath = resolvePathFactory(getCwd, isAbsolutePath)
--- src/funcdefs.ts
+++ src/funcdefs.ts
@@ -1,4 +1,4 @@
-import { resolveAsUrl, resolvePath } from "./deps.ts"
+import { isArray, isString, object_entries, object_fromEntries, promise_all, resolveAsUrl, resolvePath, type esbuild } from "./deps.ts"
 
 
 /** infer the directory path of some file as a url.
@@ -8,4 +8,87 @@ export const getDirUrlFromFile = (file_path: string = "./file.txt"): URL => {
 	// we don't simply do `resolvePath(file_path, "./")` because `resolvePath` is incapable of handling jsr/npm package names.
 	// on the other hand, I've built `resolveAsUrl` such that it comprehends package names and understands what the "root" directory of a package is.
 	return resolveAsUrl("./", resolvePath(file_path))
+}
+
+export type predictEsbuildOutputPathConfig = Omit<esbuild.BuildOptions, "bundle" | "write" | "metafile" | "entryPoints">
+export type predictEsbuildOutputPathsConfig<
+	T extends NonNullable<esbuild.BuildOptions["entryPoints"]>
+> = Omit<esbuild.BuildOptions & { entryPoints: T }, "bundle" | "write" | "metafile">
+type Inout = { in: string, out?: string }
+type InoutList = Array<Inout>
+
+const required_config: esbuild.BuildOptions = { bundle: false, write: false, metafile: true }
+
+export const predictEsbuildOutputPath = async (
+	build: typeof esbuild.build,
+	config: predictEsbuildOutputPathConfig,
+	entrypoint: string | Inout,
+): Promise<string> => {
+	const entryPoints = [entrypoint] as (string[] | Inout)
+	const result = await build({
+		...config,
+		...required_config,
+		entryPoints
+	})
+	const output_paths = object_entries(result.metafile!.outputs)
+		.filter(([output_path, metadata]) => {
+			return metadata.entryPoint !== undefined
+		})
+		.map(([output_path, metadata]) => (output_path))
+	// if there is more than one output path that has a non-undefined `metadata.entryPoint`, then we've run into a problem,
+	// because we now don't know which output path truly corresponds to our initial single input.
+	if (output_paths.length !== 1) {
+		throw new Error(`expected there to be exactly ONE output with \`metadata.entryPoint\`, but found amount: "${output_paths.length}"\n\tgiven entrypoint: "${entrypoint}"`)
+	}
+	return output_paths.pop()!
+}
+
+const entryPointsToInoutList = <
+	T extends NonNullable<esbuild.BuildOptions["entryPoints"]>
+>(entrypoints: T): InoutList => {
+	if (!isArray(entrypoints)) {
+		return object_entries(entrypoints).map(([input, output]) => ({
+			in: input,
+			out: output
+		}))
+	}
+	return (entrypoints).map((entry: string | Inout) => (
+		isString(entry)
+			? { in: entry, out: undefined }
+			: entry
+	))
+}
+
+export const predictEsbuildOutputPaths = async <
+	T extends NonNullable<esbuild.BuildOptions["entryPoints"]>
+>(
+	build: typeof esbuild.build,
+	config: predictEsbuildOutputPathsConfig<T>,
+): Promise<T> => {
+	// the logic inside of this function is just for preserving the type of entry point `T` provided by the user,
+	// and having the output also be of the same type.
+	const
+		{ entryPoints, ...rest_config } = config,
+		entrypoints_is_record = !isArray(entryPoints),
+		entry_points_list = entryPointsToInoutList(entryPoints),
+		input_and_output_paths_promise = entry_points_list.map(async (entry: Inout): Promise<Required<Inout>> => {
+			const
+				entrypoint = entry.out ? entry as Required<Inout> : entry.in,
+				output_path = await predictEsbuildOutputPath(build, rest_config, entrypoint)
+			return {
+				in: entry.in,
+				out: output_path,
+			}
+		}),
+		input_and_output_paths = await promise_all(input_and_output_paths_promise)
+	if (entrypoints_is_record) {
+		return object_fromEntries(input_and_output_paths.map(
+			({ in: input_path, out: output_path }) => ([input_path, output_path])
+		)) as T
+	}
+	return input_and_output_paths.map((input_and_output, index) => {
+		return entry_points_list[index].out
+			? input_and_output
+			: input_and_output.out
+	}) as T
 }
