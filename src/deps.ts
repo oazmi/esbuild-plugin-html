46a005edd1064f6e002fc52de63b135e9eab5ba3
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-12-02 20:17:58 -0500

    modularize the new javascript-code approach to bundling html files by using the `GenericLoader` semi-abstract class from my new `jsr:@oazmi/esbuild-generic-loader` library. - add the new `HtmlLoader` class to `/src/loader.ts`.   it is capable of parsing both, linked and inline dependencies.   however, for the time being, it will only unparse (merge back) the linked dependencies.   this is because we don't currently have a way to virtually store the contents of the inlined/innerhtml text content of the dependency element. - in `/src/deps.ts`, "fix" an issue where `isAbsolutePath` would return `true` if the segment was either an empty string, or `undefined`.

--- src/deps.ts
+++ src/deps.ts
@@ -2,10 +2,20 @@ import { forbiddenBaseUriSchemes, getUriScheme, resolvePathFactory, uriProtocolS
 
 
 export { DOMParser } from "jsr:@b-fuze/deno-dom@0.1.48"
-export { object_entries, object_fromEntries, object_keys, object_values, promise_all, json_parse, json_stringify } from "jsr:@oazmi/kitchensink@0.8.5/alias"
-export { getUriScheme, joinPaths, relativePath, resolveAsUrl } from "jsr:@oazmi/kitchensink@0.8.5/pathman"
+export { GenericLoader, zipArrays, zipArraysMapperFactory, type ContentDependencies, type GenericLoaderConfig } from "jsr:@oazmi/esbuild-generic-loader@0.1.1"
+export { json_parse, json_stringify, object_entries, object_fromEntries, object_keys, object_values, promise_all } from "jsr:@oazmi/kitchensink@0.8.5/alias"
+export { commonPathReplace, ensureEndSlash, getUriScheme, joinPaths, relativePath, resolveAsUrl } from "jsr:@oazmi/kitchensink@0.8.5/pathman"
 export { isArray, isString } from "jsr:@oazmi/kitchensink@0.8.5/struct"
 export type * as esbuild from "npm:esbuild"
+
+/** flags used for minifying (or eliminating) debugging logs and asserts, when an intelligent bundler, such as `esbuild`, is used. */
+export const enum DEBUG {
+	LOG = 0,
+	ASSERT = 1,
+	ERROR = 1,
+	PRODUCTION = 1,
+	MINIFY = 1,
+}
 
 // DONE: in the future, we will have to wrap `getUriScheme` and export a different `getUriScheme` that understands our custom `inline://` uri-scheme
 uriProtocolSchemeMap.push(
@@ -16,9 +26,14 @@ forbiddenBaseUriSchemes.push("link" as any, "inline" as any)
 
 // below is a custom path segment's absoluteness test function that will identify all `UriScheme` segments that are not "relative" as absolute paths,
 export const isAbsolutePath = (segment: string) => {
-	return getUriScheme(segment) !== "relative"
+	const scheme = getUriScheme(segment) ?? "relative"
+	return scheme !== "relative"
 }
 
 export const
 	getCwd = () => Deno.cwd(),
 	resolvePath = resolvePathFactory(getCwd, isAbsolutePath)
+
+export const
+	textEncoder = new TextEncoder(),
+	textDecoder = new TextDecoder()
